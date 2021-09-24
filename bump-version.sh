6725aaae17c823b682ce991cb237c07bcf7116fb
Zoran Lazarevic <lazarevic.zoki91@gmail.com>
2021-09-24 19:19:19 +0200

    update version in files

--- bump-version.sh
+++ bump-version.sh
@@ -20,27 +20,11 @@ elif [[ "$CURRENT_BRANCH_LOWER_CASE" =~ ^release/.* ]]; then
     VERSION=major
 fi
 
-echo $VERSION
-
 ## move to project root
 cd "$(git rev-parse --show-toplevel)"
 
-echo "hello"
-
-echo "1.2.3" | grep -P "\d+"
-
-git show remotes/origin/$GITHUB_BASE_REF:package.json | grep -P '"version": ".*?"' | grep -Po "\d+"
-
-echo "printed out the version"
-
 ## increment version number from $GITHUB_BASE_REF
 VALUES=($(git show remotes/origin/$GITHUB_BASE_REF:package.json | grep -P '"version": ".*?"' | grep -Po "\d+"))
-echo "about to print out new version"
-echo $(join . ${VALUES[@]})
-
-CURRENT_VALUES=($(grep -P '"version": ".*?"' package.json | grep -Po "\d+"))
-CURRENT_VERSION=$(join . ${CURRENT_VALUES[@]})
-echo $CURRENT_VERSION
 if [ "$VERSION" == "major" ]; then
   VALUES[0]=$((VALUES[0] + 1))
   VALUES[1]=0
@@ -52,9 +36,9 @@ else
   VALUES[2]=$((VALUES[2] + 1))
 fi
 NEW_TARGET_VERSION=$(join . ${VALUES[@]})
-echo $NEW_TARGET_VERSION
-echo "current version $CURRENT_VERSION - new version $NEW_TARGET_VERSION"
 
+CURRENT_VERSION_VALUES=($(grep -P '"version": ".*?"' package.json | grep -Po "\d+"))
+CURRENT_VERSION=$(join . ${CURRENT_VERSION_VALUES[@]})
 if [[ "$NEW_TARGET_VERSION" == "$CURRENT_VERSION" ]]; then
     # current version is already set to target version, exiting script
     echo "same version"
@@ -62,17 +46,17 @@ if [[ "$NEW_TARGET_VERSION" == "$CURRENT_VERSION" ]]; then
 fi
 
 echo "different version, proceed to bumping relevant files"
-## update package.json and package-lock.json with new version
-# awk -v version="$NEW_TARGET_VERSION" \
-#   '/\"version\": \".*?\"/ && count < 1 { gsub("\"version\": \".*?\"", "\"version\": \""version"\""); count++ } {print}' package.json > \
-#   package.json_tmp && \
-#   mv package.json_tmp package.json
-# if [[ -f "package-lock.json" ]]; then
-#   awk -v version="$NEW_TARGET_VERSION" \
-#     '/\"version\": \".*?\"/ && count < 1 { gsub("\"version\": \".*?\"", "\"version\": \""version"\""); count++ } {print}' package-lock.json > \
-#     package-lock.json_tmp && \
-#     mv package-lock.json_tmp package-lock.json
-# fi
+# update package.json and package-lock.json with new version
+awk -v version="$NEW_TARGET_VERSION" \
+  '/\"version\": \".*?\"/ && count < 1 { gsub("\"version\": \".*?\"", "\"version\": \""version"\""); count++ } {print}' package.json > \
+  package.json_tmp && \
+  mv package.json_tmp package.json
+if [[ -f "package-lock.json" ]]; then
+  awk -v version="$NEW_TARGET_VERSION" \
+    '/\"version\": \".*?\"/ && count < 1 { gsub("\"version\": \".*?\"", "\"version\": \""version"\""); count++ } {print}' package-lock.json > \
+    package-lock.json_tmp && \
+    mv package-lock.json_tmp package-lock.json
+fi
 
 git config user.email "41898282+github-actions[bot]@users.noreply.github.com"
 git config user.name "$GITHUB_ACTOR"
