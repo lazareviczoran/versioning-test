7b1d463d1a56a23b9d2dd5bd41bc68f8fabba4fe
Zoran Lazarevic <lazarevic.zoki91@gmail.com>
2021-09-24 18:47:49 +0200

    more debugging

--- bump-version.sh
+++ bump-version.sh
@@ -25,12 +25,13 @@ echo $VERSION
 ## move to project root
 cd "$(git rev-parse --show-toplevel)"
 
-git branch -a
-
 git show remotes/origin/$GITHUB_BASE_REF:package.json | grep -E '"version": ".*?"' | grep -Eo "\d+"
+
+echo "printed out the version"
 
 ## increment version number from $GITHUB_BASE_REF
 VALUES=($(git show remotes/origin/$GITHUB_BASE_REF:package.json | grep -E '"version": ".*?"' | grep -Eo "\d+"))
+echo "about to print out new version"
 echo $(join . ${VALUES[@]})
 
 CURRENT_VALUES=($(grep -E '"version": ".*?"' package.json | grep -Eo "\d+"))
