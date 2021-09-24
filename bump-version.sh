26121d057e71a9f4439f096365f6884a7a34ab3d
Zoran Lazarevic <lazarevic.zoki91@gmail.com>
2021-09-24 15:47:45 +0200

    use base ref instead of "develop"

--- bump-version.sh
+++ bump-version.sh
@@ -25,8 +25,8 @@ echo $VERSION
 ## move to project root
 cd "$(git rev-parse --show-toplevel)"
 
-## increment version number from develop
-VALUES=($(git show develop:package.json | grep -E '"version": ".*?"' | grep -Eo "\d+"))
+## increment version number from $GITHUB_BASE_REF
+VALUES=($(git show $GITHUB_BASE_REF:package.json | grep -E '"version": ".*?"' | grep -Eo "\d+"))
 CURRENT_VALUES=($(grep -E '"version": ".*?"' package.json | grep -Eo "\d+"))
 CURRENT_VERSION=$(join . ${CURRENT_VALUES[@]})
 echo $CURRENT_VERSION
