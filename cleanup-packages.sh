53839f8717a64f8f93a60e66b9cc0e306f8b21f7
Zoran Lazarevic <lazarevic.zoki91@gmail.com>
2021-09-27 16:27:20 +0200

    debug cleanup script

--- cleanup-packages.sh
+++ cleanup-packages.sh
@@ -24,22 +24,29 @@ EOF
 PULL_REQUEST_ID=$(jq --raw-output .pull_request.number "$GITHUB_EVENT_PATH")
 TARGET_VERSION_NAME_PREFIX="0.0.0-PR$PULL_REQUEST_ID"
 
-ACTIVE_VERSIONS_ITEMS=$(curl -s \
+curl -s \
     -X POST \
     -H "Accept: application/vnd.github.v3+json" \
     -H "Authorization: bearer $GITHUB_TOKEN" \
     -d '{"query":"'"$query"'"}' \
-    https://api.github.com/graphql | jq -r '.data.repository.packages.nodes[0].versions.nodes[]')
+    https://api.github.com/graphql
 
-FILTERED_VERSIONS_ITEMS=$(echo $ACTIVE_VERSIONS_ITEMS \
-                            | jq -r ".|select(.version | startswith('$TARGET_VERSION_NAME_PREFIX'))" \
-                            | jq -r '.id')
+# ACTIVE_VERSIONS_ITEMS=$(curl -s \
+#     -X POST \
+#     -H "Accept: application/vnd.github.v3+json" \
+#     -H "Authorization: bearer $GITHUB_TOKEN" \
+#     -d '{"query":"'"$query"'"}' \
+#     https://api.github.com/graphql | jq -r '.data.repository.packages.nodes[0].versions.nodes[]')
 
-VERSION_IDS_TO_DELETE=($(echo $FILTERED_VERSIONS_ITEMS))
-for id in "${VERSION_IDS_TO_DELETE[@]}"; do
-    curl -X POST \
-        -H "Accept: application/vnd.github.package-deletes-preview+json" \
-        -H "Authorization: bearer $GITHUB_TOKEN" \
-        -d "{\"query\":\"mutation { deletePackageVersion(input:{packageVersionId:\\\"$id\\\"}) { success }}\"}"
-        https://api.github.com/graphql
-done
+# FILTERED_VERSIONS_ITEMS=$(echo $ACTIVE_VERSIONS_ITEMS \
+#                             | jq -r ".|select(.version | startswith('$TARGET_VERSION_NAME_PREFIX'))" \
+#                             | jq -r '.id')
+
+# VERSION_IDS_TO_DELETE=($(echo $FILTERED_VERSIONS_ITEMS))
+# for id in "${VERSION_IDS_TO_DELETE[@]}"; do
+#     curl -X POST \
+#         -H "Accept: application/vnd.github.package-deletes-preview+json" \
+#         -H "Authorization: bearer $GITHUB_TOKEN" \
+#         -d "{\"query\":\"mutation { deletePackageVersion(input:{packageVersionId:\\\"$id\\\"}) { success }}\"}"
+#         https://api.github.com/graphql
+# done
