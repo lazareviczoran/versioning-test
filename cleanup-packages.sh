34b6137c49810f239bd6d531343c534615ea3d7d
Zoran Lazarevic <lazarevic.zoki91@gmail.com>
2021-09-27 16:30:02 +0200

    more debugging

--- cleanup-packages.sh
+++ cleanup-packages.sh
@@ -24,23 +24,20 @@ EOF
 PULL_REQUEST_ID=$(jq --raw-output .pull_request.number "$GITHUB_EVENT_PATH")
 TARGET_VERSION_NAME_PREFIX="0.0.0-PR$PULL_REQUEST_ID"
 
-curl -s \
+ACTIVE_VERSIONS_ITEMS=$(curl -s \
     -X POST \
     -H "Accept: application/vnd.github.v3+json" \
     -H "Authorization: bearer $GITHUB_TOKEN" \
     -d '{"query":"'"$query"'"}' \
-    https://api.github.com/graphql
+    https://api.github.com/graphql | jq -r '.data.repository.packages.nodes[0].versions.nodes[]')
 
-# ACTIVE_VERSIONS_ITEMS=$(curl -s \
-#     -X POST \
-#     -H "Accept: application/vnd.github.v3+json" \
-#     -H "Authorization: bearer $GITHUB_TOKEN" \
-#     -d '{"query":"'"$query"'"}' \
-#     https://api.github.com/graphql | jq -r '.data.repository.packages.nodes[0].versions.nodes[]')
+echo $ACTIVE_VERSIONS_ITEMS
 
-# FILTERED_VERSIONS_ITEMS=$(echo $ACTIVE_VERSIONS_ITEMS \
-#                             | jq -r ".|select(.version | startswith('$TARGET_VERSION_NAME_PREFIX'))" \
-#                             | jq -r '.id')
+FILTERED_VERSIONS_ITEMS=$(echo $ACTIVE_VERSIONS_ITEMS \
+                            | jq -r ".|select(.version | startswith('$TARGET_VERSION_NAME_PREFIX'))" \
+                            | jq -r '.id')
+
+echo $FILTERED_VERSIONS_ITEMS
 
 # VERSION_IDS_TO_DELETE=($(echo $FILTERED_VERSIONS_ITEMS))
 # for id in "${VERSION_IDS_TO_DELETE[@]}"; do
